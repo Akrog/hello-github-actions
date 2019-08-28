a0376fb00654ee45beb5da9d158f722f8c80241d
Gorka Eguileor <geguileo@redhat.com>
2019-08-28 15:29:15 +0200

    Display event and payload

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,3 +1,5 @@
-#!/bin/sh -l
+#!/bin/bash
 
-sh -c "echo Hello world my name is $MY_NAME"
+echo Hello world my name is $MY_NAME
+echo Event is: $GITHUB_EVENT_NAME
+echo Event payload is: "`cat $GITHUB_EVENT_PATH`"
